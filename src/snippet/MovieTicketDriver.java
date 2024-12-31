a56f61538fb78974f23de274f3af843e532cddaf
Rhugved Takalkar <rrtask6@gmail.com>
2024-12-31 08:40:35 +0000

    Movie Class Creation and Implementation

---
+++ src/snippet/MovieTicketDriver.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class MovieTicketDriver {
+    public static void main(String[] args) {
+
+        //Movie m = new Movie("Race 3", "Action", 4.9, 120, "It's a good movie of racing");
+        //m.insertMovie();
+
+        Movie m = new Movie();
+        m.showMovies();
+    }
+
+}
